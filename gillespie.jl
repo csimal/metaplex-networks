b038a4ab326446ada540f906c124c8450af30da7
csimal <cedric.simal@student.unamur.be>
2020-02-04 12:54:03 +0000

    Fixed + tests

--- gillespie.jl
+++ gillespie.jl
@@ -10,7 +10,7 @@ struct ReactionSystem
     update_a::Vector{Vector{Integer}}
 end
 
-function gillespie_algorithm(rs::ReactionSystem, Xi, tmax = 100.0, nmax = 500)
+function gillespie_algorithm(rs::ReactionSystem, Xi; tmax = 100.0, nmax = 500)
     t = 0.0
     n = 0
     ts = Vector{typeof(t)}(undef, nmax)
@@ -28,17 +28,19 @@ function gillespie_algorithm(rs::ReactionSystem, Xi, tmax = 100.0, nmax = 500)
             μ += 1
             s += a[μ]
         end
+        # update X
         for (i,j) in rs.update_X[μ]
             X[i] += j
         end
+        # update a
         for ν in rs.update_a[μ]
             a[ν] = rs.c[ν]*(rs.h[ν](X))
         end
         a0 = sum(a) # recompute the entire sum to avoid rounding errors
-        t += τ
-        n += 1
         ts[n+1] = t
         Xs[n+1,:] = X
+        t += τ
+        n += 1
     end
     return ts[1:n], Xs[1:n,:]
 end
@@ -57,27 +59,58 @@ function LotkaVolterra(c)
      X -> X[1]*X[2],
      X -> X[2]],
     [[(1,1)],[(1,-1),(2,1)],[(2,-1)]],
-    [[1],[1,2],[2]]
+    [[1],[1,2,3],[2,3]]
+    )
+end
+
+function ClosedLotkaVolterra(c)
+    rs = ReactionSystem(3,3,c,
+    [X -> X[1]*X[3],
+     X -> X[1]*X[2],
+     X -> X[2]],
+    [[(1,1),(3,-1)],[(1,-1),(2,1)],[(2,-1)]],
+    [[1],[1,2,3],[2,3]]
     )
 end
 
 function Brusselator(c)
-    rs = ReactionSystem(4,4,c
+    rs = ReactionSystem(4,4,c,
     [X -> X[3],
      X -> X[4]*X[1],
-     X -> X[2]*X[1](X[1]-1)/2,
-     X -> X[2]],
-     [[(1,1)],[(1,-1),(2,1)],[(1,-2),(2,2)],[(1,-1)]],
-     [[2,3,4],[2,3,4],[2,3,4],[2,3,4]]
+     X -> X[2]*X[1]*(X[1]-1)/2,
+     X -> X[1]],
+     [[(1,1)], [(1,-1),(2,1)], [(1,1),(2,-1)], [(1,-1)]],
+     [[2,3,4], [2,3,4], [2,3,4], [2,3,4]]
     )
 end
 
-si = SI(0.5)
-
-ts, Xs = gillespie_algorithm(si, [100,1])
 
 using Plots
 
+si = SI(0.5)
+ts, Xs = gillespie_algorithm(si, [100,1])
 plot(ts, Xs[:,1], line=:steppre)
 scatter!(ts, Xs[:,1], markersize=1)
 plot!(ts, 100*exp.(-0.5*ts))
+
+X0 = 100000
+lv = LotkaVolterra([10/X0,0.01,10])
+
+ts, Xs = gillespie_algorithm(lv, [1000,1000,X0], nmax = 10^6)
+
+plot(ts, Xs[:,1], line=:steppre)
+plot!(ts, Xs[:,2], line=:steppre)
+
+plot(Xs[:,1],Xs[:,2], linewidth=0.1, label="")
+
+
+X1 = 100000
+X2 = 100000
+br = Brusselator([5000/X0, 50/X2, 0.00005, 5])
+
+ts, Xs = gillespie_algorithm(br, [1000,2000,X1,X2], tmax=10000.0, nmax=2*10^6)
+
+plot(ts, Xs[:,1], line=:steppre)
+plot!(ts, Xs[:,2], line=:steppre)
+
+plot(Xs[:,1],Xs[:,2], linewidth=0.1, label="")
